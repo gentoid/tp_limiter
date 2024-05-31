a3c18f4802dd7222a024b3a68144665f55474df5
Viktor Lazarev <taurus101v@gmail.com>
2024-05-31 21:47:53 +0200

    Copy some code from the stft example of nih-plug. Add gui

---
+++ src/gui.rs
@@ -0,0 +1,107 @@
+use std::sync::{atomic, Arc};
+
+use nih_plug::{context::gui::GuiContext, editor::Editor};
+use nih_plug_iced::*;
+
+use crate::{TpLimiterParams, Values};
+
+pub(crate) fn default_state() -> Arc<IcedState> {
+    IcedState::from_size(300, 180)
+}
+
+pub(crate) fn create(
+    editor_state: Arc<IcedState>,
+    params: Arc<TpLimiterParams>,
+    values: Arc<Values>,
+) -> Option<Box<dyn Editor>> {
+    create_iced_editor::<TpLimiterEditor>(editor_state, (params, values))
+}
+
+struct TpLimiterEditor {
+    params: Arc<TpLimiterParams>,
+    context: Arc<dyn GuiContext>,
+
+    values: Arc<Values>,
+    slider_state: widgets::param_slider::State,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    ParamUpdate(widgets::ParamMessage),
+}
+
+impl IcedEditor for TpLimiterEditor {
+    type Executor = executor::Default;
+
+    type Message = Message;
+
+    type InitializationFlags = (Arc<TpLimiterParams>, Arc<Values>);
+
+    fn new(
+        (params, values): Self::InitializationFlags,
+        context: Arc<dyn GuiContext>,
+    ) -> (Self, Command<Self::Message>) {
+        let editor = TpLimiterEditor {
+            params,
+            context,
+            values,
+            slider_state: Default::default(),
+        };
+
+        (editor, Command::none())
+    }
+
+    fn context(&self) -> &dyn GuiContext {
+        self.context.as_ref()
+    }
+
+    fn update(
+        &mut self,
+        _window: &mut WindowQueue,
+        message: Self::Message,
+    ) -> Command<Self::Message> {
+        match message {
+            Message::ParamUpdate(msg) => self.handle_param_message(msg),
+        }
+
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<'_, Self::Message> {
+        Column::new()
+            .align_items(Alignment::Center)
+            .push(
+                Text::new("TP Limiter")
+                    .height(25.into())
+                    .width(Length::Fill)
+                    .horizontal_alignment(alignment::Horizontal::Center)
+                    .vertical_alignment(alignment::Vertical::Center),
+            )
+            .push(
+                widgets::ParamSlider::new(&mut self.slider_state, &self.params.gain)
+                    .map(Message::ParamUpdate),
+            )
+            .push(
+                Text::new(format!("Min value: {}", self.values.min.load(atomic::Ordering::Relaxed)))
+                    .height(25.into())
+                    .width(Length::Fill)
+                    .horizontal_alignment(alignment::Horizontal::Center)
+                    .vertical_alignment(alignment::Vertical::Center),
+            )
+            .push(
+                Text::new(format!("Max value: {}", self.values.max.load(atomic::Ordering::Relaxed)))
+                    .height(25.into())
+                    .width(Length::Fill)
+                    .horizontal_alignment(alignment::Horizontal::Center)
+                    .vertical_alignment(alignment::Vertical::Center),
+            )
+            .push(
+                Text::new(format!("ABS value: {}", self.values.abs.load(atomic::Ordering::Relaxed)))
+                    .height(25.into())
+                    .width(Length::Fill)
+                    .horizontal_alignment(alignment::Horizontal::Center)
+                    .vertical_alignment(alignment::Vertical::Center),
+            )
+            .into()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,91 @@
-use nih_plug::prelude::*;
-use rubato::{FftFixedInOut, Resampler};
-use std::sync::Arc;
-use utils::prepare_resampler;
+use nih_plug::{prelude::*, util::StftHelper};
+use nih_plug_iced::IcedState;
+use realfft::{
+    num_complex::{Complex32, ComplexFloat},
+    ComplexToReal, RealFftPlanner, RealToComplex,
+};
+use std::sync::{atomic, Arc};
 
-mod utils;
+mod gui;
 
-// This is a shortened version of the gain example with most comments removed, check out
-// https://github.com/robbert-vdh/nih-plug/blob/master/plugins/examples/gain/src/lib.rs to get
-// started
+/// The size of the windows we'll process at a time.
+const WINDOW_SIZE: usize = 64;
+/// The length of the filter's impulse response.
+const FILTER_WINDOW_SIZE: usize = 33;
+/// The length of the FFT window we will use to perform FFT convolution. This includes padding to
+/// prevent time domain aliasing as a result of cyclic convolution.
+const FFT_WINDOW_SIZE: usize = WINDOW_SIZE + FILTER_WINDOW_SIZE - 1;
 
-const RESAMPLING_ERROR: &'static str = "Error during resampling";
+/// The gain compensation we need to apply for the STFT process.
+const GAIN_COMPENSATION: f32 = 1.0 / FFT_WINDOW_SIZE as f32;
 
 struct TpLimiter {
     params: Arc<TpLimiterParams>,
-    audio_io_layout: AudioIOLayout,
-    sample_rate: usize,
-    oversampling: usize,
-    upsampler: FftFixedInOut<f32>,
-    downsampler: FftFixedInOut<f32>,
+
+    /// An adapter that performs most of the overlap-add algorithm for us.
+    stft: StftHelper,
+
+    /// The algorithm for the FFT operation.
+    r2c_plan: Arc<dyn RealToComplex<f32>>,
+    /// The algorithm for the IFFT operation.
+    c2r_plan: Arc<dyn ComplexToReal<f32>>,
+    /// The output of our real->complex FFT.
+    complex_fft_buffer: Vec<Complex32>,
+
+    values: Arc<Values>,
+}
+
+#[derive(Default)]
+struct Values {
+    min: AtomicF32,
+    max: AtomicF32,
+    abs: AtomicF32,
 }
 
 #[derive(Params)]
 struct TpLimiterParams {
-    /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
-    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
-    /// parameters are exposed to the host in the same order they were defined. In this case, this
-    /// gain parameter is stored as linear gain while the values are displayed in decibels.
+    #[persist = "gui-state"]
+    gui_state: Arc<IcedState>,
+    
     #[id = "gain"]
     pub gain: FloatParam,
 }
 
 impl Default for TpLimiter {
     fn default() -> Self {
-        let sample_rate = 44100;
-        let oversampling = 16;
-        let high_sample_rate = sample_rate * oversampling;
-        let audio_io_layout = AudioIOLayout::default();
+        let mut planner = RealFftPlanner::new();
+        let r2c_plan = planner.plan_fft_forward(FFT_WINDOW_SIZE);
+        let c2r_plan = planner.plan_fft_inverse(FFT_WINDOW_SIZE);
+        let mut real_fft_buffer = r2c_plan.make_input_vec();
+        let mut complex_fft_buffer = r2c_plan.make_output_vec();
+
+        // Build a super simple low-pass filter from one of the built in window functions
+        let mut filter_window = util::window::hann(FILTER_WINDOW_SIZE);
+        // And make sure to normalize this so convolution sums to 1
+        let filter_normalization_factor = filter_window.iter().sum::<f32>().recip();
+        for sample in &mut filter_window {
+            *sample *= filter_normalization_factor;
+        }
+        real_fft_buffer[0..FILTER_WINDOW_SIZE].copy_from_slice(&filter_window);
+
+        // RustFFT doesn't actually need a scratch buffer here, so we'll pass an empty buffer
+        // instead
+        r2c_plan
+            .process_with_scratch(&mut real_fft_buffer, &mut complex_fft_buffer, &mut [])
+            .unwrap();
         Self {
             params: Arc::new(TpLimiterParams::default()),
-            audio_io_layout,
-            sample_rate,
-            oversampling,
-            upsampler: prepare_resampler(
-                sample_rate,
-                high_sample_rate,
-                &audio_io_layout,
-                "Couldn't create default upsampler",
-            ),
-            downsampler: prepare_resampler(
-                high_sample_rate,
-                sample_rate,
-                &audio_io_layout,
-                "Couldn't create default downsampler",
-            ),
+
+            // We'll process the input in `WINDOW_SIZE` chunks, but our FFT window is slightly
+            // larger to account for time domain aliasing so we'll need to add some padding ot each
+            // block.
+            stft: StftHelper::new(2, WINDOW_SIZE, FFT_WINDOW_SIZE - WINDOW_SIZE),
+
+            r2c_plan,
+            c2r_plan,
+            complex_fft_buffer,
+
+            values: Arc::new(Values::default())
         }
     }
 }
@@ -60,6 +93,7 @@ impl Default for TpLimiter {
 impl Default for TpLimiterParams {
     fn default() -> Self {
         Self {
+            gui_state: gui::default_state(),
             // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
             // to treat these kinds of parameters as if we were dealing with decibels. Storing this
             // as decibels is easier to work with, but requires a conversion for every sample.
@@ -128,38 +162,31 @@ impl Plugin for TpLimiter {
         self.params.clone()
     }
 
+    fn editor(&mut self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
+        gui::create(self.params.gui_state.clone(), self.params.clone(), self.values.clone())
+    }
+
     fn initialize(
         &mut self,
-        audio_io_layout: &AudioIOLayout,
-        buffer_config: &BufferConfig,
-        _context: &mut impl InitContext<Self>,
+        _audio_io_layout: &AudioIOLayout,
+        _buffer_config: &BufferConfig,
+        context: &mut impl InitContext<Self>,
     ) -> bool {
-        self.audio_io_layout = audio_io_layout.clone();
-        self.sample_rate = buffer_config.sample_rate as usize;
-
-        let high_sample_rate = self.sample_rate * self.oversampling;
-        self.upsampler = prepare_resampler(
-            self.sample_rate,
-            high_sample_rate,
-            &self.audio_io_layout,
-            "Culdn't init upsampler",
-        );
-        self.downsampler = prepare_resampler(
-            high_sample_rate,
-            self.sample_rate,
-            &self.audio_io_layout,
-            "Culdn't init downsampler",
-        );
-
-        // Resize buffers and perform other potentially expensive initialization operations here.
-        // The `reset()` function is always called right after this function. You can remove this
-        // function if you do not need it.
+        // The plugin's latency consists of the block size from the overlap-add procedure and half
+        // of the filter kernel's size (since we're using a linear phase/symmetrical convolution
+        // kernel)
+        context.set_latency_samples(self.stft.latency_samples() + (FILTER_WINDOW_SIZE as u32 / 2));
         true
     }
 
     fn reset(&mut self) {
         // Reset buffers and envelopes here. This can be called from the audio thread and may not
         // allocate. You can remove this function if you do not need it.
+
+        // Normally we'd also initialize the STFT helper for the correct channel count here, but we
+        // only do stereo so that's not necessary. Setting the block size also zeroes out the
+        // buffers.
+        self.stft.set_block_size(WINDOW_SIZE);
     }
 
     fn process(
@@ -168,25 +195,40 @@ impl Plugin for TpLimiter {
         _aux: &mut AuxiliaryBuffers,
         _context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
-        match self
-            .upsampler
-            .process(&buffer.as_slice_immutable(), None)
-            .and_then(|wave| self.downsampler.process(&wave, None))
-        {
-            Err(err) => {
-                nih_error!("{RESAMPLING_ERROR}: {err}");
-                ProcessStatus::Error(RESAMPLING_ERROR)
-            }
-            Ok(wave) => {
-                for (a, b) in buffer.as_slice().into_iter().zip(wave) {
-                    for (a, b) in a.iter_mut().zip(b) {
-                        *a = b;
-                    }
+        self.stft
+            .process_overlap_add(buffer, 1, |_channel_idx, real_fft_buffer| {
+                // Forward FFT, `real_fft_buffer` already is already padded with zeroes, and the
+                // padding from the last iteration will have already been added back to the start of
+                // the buffer
+                self.r2c_plan
+                    .process_with_scratch(real_fft_buffer, &mut self.complex_fft_buffer, &mut [])
+                    .unwrap();
+
+                let mut min: f32 = 0.0;
+                let mut max: f32 = 0.0;
+                let mut abs: f32 = 0.0;
+                // As per the convolution theorem we can simply multiply these two buffers. We'll
+                // also apply the gain compensation at this point.
+                for fft_bin in self.complex_fft_buffer.iter_mut()
+                {
+                    *fft_bin *= GAIN_COMPENSATION;
+                    min = min.min(fft_bin.re);
+                    max = max.max(fft_bin.re);
+                    abs = abs.max(fft_bin.abs());
                 }
 
-                ProcessStatus::Normal
-            }
-        }
+                self.values.min.store(self.values.min.load(atomic::Ordering::Relaxed).min(min), atomic::Ordering::Relaxed);
+                self.values.max.store(self.values.max.load(atomic::Ordering::Relaxed).max(max), atomic::Ordering::Relaxed);
+                self.values.abs.store(self.values.abs.load(atomic::Ordering::Relaxed).max(abs), atomic::Ordering::Relaxed);
+
+                // Inverse FFT back into the scratch buffer. This will be added to a ring buffer
+                // which gets written back to the host at a one block delay.
+                self.c2r_plan
+                    .process_with_scratch(&mut self.complex_fft_buffer, real_fft_buffer, &mut [])
+                    .unwrap();
+            });
+
+        ProcessStatus::Normal
     }
 }
 
